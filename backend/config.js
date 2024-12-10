0306dab8f8ea1e4e140160c4a6d525fcc822d98c
Geoffrey Gracia <grg7576@rit.edu>
2024-12-10 15:16:36 -0500

    starting this

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+   process.env.MONGODB_URI || 'mongodb://localhost:27107/projthree';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
